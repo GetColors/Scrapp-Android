1b0ab0a6ce263bd14d1de20b5e50231322ec38be
Camilo Alaniz <kmilo93sd@gmail.com>
2018-11-28 17:24:29 -0300

    now the application is closed when double click

--- app/src/main/java/cl/getcolors/scrapp/MainActivity.kt
+++ app/src/main/java/cl/getcolors/scrapp/MainActivity.kt
@@ -2,10 +2,14 @@ package cl.getcolors.scrapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +18,20 @@ class MainActivity : AppCompatActivity() {
 
     fun redirectToSignin(view : View){
 
-        val signIn = Intent(this, Signin::class.java);
-        startActivity(signIn);
+        val signIn = Intent(this, Signin::class.java)
+        startActivity(signIn)
     }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            finishAffinity()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
+    }
+
 }
--- app/src/main/java/cl/getcolors/scrapp/Signin.kt
+++ app/src/main/java/cl/getcolors/scrapp/Signin.kt
@@ -3,9 +3,13 @@ package cl.getcolors.scrapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.Toast
 
 class Signin : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +18,20 @@ class Signin : AppCompatActivity() {
 
     fun redirectToRegister(view : View){
 
-        val register = Intent(this, MainActivity::class.java);
-        startActivity(register);
+        val register = Intent(this, MainActivity::class.java)
+        startActivity(register)
     }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            finishAffinity()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
+    }
+
 }
